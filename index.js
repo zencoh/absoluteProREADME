993a11d755074971e9c1d83658e414e5e59dda9f
Cole Hartkopp <chsoccer4@outlook.com>
2023-02-12 12:02:28 -0600

    created questions for user input

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description about your project'
+    },
+    {
+        type: 'input',
+        name: 'instal',
+        message: 'Give installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Give usage information'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Give contribution guidelines'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Give testing instuctions'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license',
+        choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your Email?'
+    }
+];
+inquirer.prompt(questions).then(answers => {
+    const { title, description, instal, usage, contributing, test, license, username, email } = answers;
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
